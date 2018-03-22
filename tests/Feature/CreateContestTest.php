924a05882ae1eb1fd4af5b47945d4a89081b71f3
Jorge Saud <jorgelsaud@gmail.com>
2018-03-22 18:45:31 -0300

    fix resolution

--- tests/Feature/CreateContestTest.php
+++ tests/Feature/CreateContestTest.php
@@ -16,52 +16,29 @@ class CreateContestTest extends TestCase
      */
     public function an_administrator_user_can_create_new_contest()
     {
-<<<<<<< HEAD
         $this->withExceptionHandling();
-    	$role = Role::whereName('Administrator')->first();
-    	$user = factory(User::class)->create();
-    	$user->roles()->attach($role->id);
-    	$this->signIn($user);
-    	$contest=[
-    		'en'=>[
-    			'topic'=>'Ocean',
-    			'description'=>'Ocean Description'
-    		],
-    		'es'=>[
-    			'topic'=>'Oceano',
-    			'description'=>'Descripcion Oceano'	
-    		],
-    		'year'=>'2000',
-            'normalCategories'=>true
-    	];
-    	$response = $this->post('/contests', $contest);
-    	$this->get($response->headers->get('Location'))
-    	->assertSee($contest['en']['topic'])
-    	->assertSee($contest['en']['description'])
-    	->assertSee((string) $contest['year']);
-
-=======
         $role = Role::whereName('Administrator')->first();
         $user = factory(User::class)->create();
         $user->roles()->attach($role->id);
         $this->signIn($user);
-        $contest = [
-            'en'=> [
-                'topic'      => 'Ocean',
-                'description'=> 'Ocean Description',
+        $contest=[
+            'en'=>[
+                'topic'=>'Ocean',
+                'description'=>'Ocean Description'
             ],
-            'es'=> [
-                'topic'      => 'Oceano',
-                'description'=> 'Descripcion Oceano',
+            'es'=>[
+                'topic'=>'Oceano',
+                'description'=>'Descripcion Oceano'
             ],
-            'year'=> '2000',
+            'year'=>'2000',
+            'normalCategories'=>true
         ];
         $response = $this->post('/contests', $contest);
         $this->get($response->headers->get('Location'))
         ->assertSee($contest['en']['topic'])
         ->assertSee($contest['en']['description'])
         ->assertSee((string) $contest['year']);
->>>>>>> 8037ea1b34ab0218a6728aab466b4c89ab550fc3
+
 
         // $this->get($response->headers->get('Location'))
     }
