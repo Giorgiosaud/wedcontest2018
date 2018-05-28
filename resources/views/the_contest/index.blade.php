df3f279e811a6f2f88d724254ce182b6d5ff38d2
Jorge Saud <jorgelsaud@gmail.com>
2018-05-27 22:19:34 -0400

    WIP

--- resources/views/the_contest/index.blade.php
+++ resources/views/the_contest/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="d-flex flex-column h-100vh align-items-center justify-content-center text-light" style="background-image:url({{$contest->background_image}})">
     <div class="container text-center my-4">
-        <img src="/{{$contest->logo_image}}" alt="{{$contest->topic}}" style="max-width:200px">
+        <img src="/storage/{{$contest->logo_image}}" alt="{{$contest->topic}}" style="max-width:200px">
         <div class="separator-wedcontest"></div>
         <h1 class="my-4">@lang('registration.registration')</h1>
         <h2 class="mb-4">{!! $contest->description !!}</h2>
