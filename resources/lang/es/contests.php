62e3ff0b6c18bc30f5cf88819f96e26460f12e1d
Jorge Saud <jorgelsaud@gmail.com>
2018-06-06 20:15:47 -0400

    some minor fixes

--- resources/lang/es/contests.php
+++ resources/lang/es/contests.php
@@ -4,7 +4,7 @@ return [
     'edit'            => 'Editar',
     'read_more'       => 'Leer Más',
     'confirm_email'   => 'Por Favor Confirma el email para poder concursar.',
-    'register_button' => 'Registrese Como Representante',
+    'register_button' => 'Registrese como representante',
     'login_button'    => 'Logueese en el Concurso',
     'topic'           => 'Tema',
     'new_contest'     => 'Crear Concurso',
