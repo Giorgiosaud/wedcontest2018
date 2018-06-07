bc13da2f69cdba42f308ab9c08ef3c2673324f02
Jorge Saud <jorgelsaud@gmail.com>
2018-06-06 23:23:48 -0400

    some minor fixes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/the_contest';
+    protected $redirectTo = '/mycontestants';
 
     /**
      * Create a new controller instance.
