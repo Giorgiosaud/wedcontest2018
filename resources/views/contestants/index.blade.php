b0ec8cc86acdf5f5f5e1bc1113952c7cd3e8611f
Jorge Saud <jorgelsaud@gmail.com>
2018-06-09 10:23:46 -0400

    some fixes

--- resources/views/contestants/index.blade.php
+++ resources/views/contestants/index.blade.php
@@ -7,11 +7,11 @@
 	</div>
 	<div class="row">
 		@foreach($contestants as $contestant)
-		<contestant-card edit-link="{{  route('contestant.edit',$contestant->id)}}" :contestant="{{$contestant}}" :key="{{$contestant->id}}"></contestant-card>
+		<contestant-card edit-link="{{  route('mycontestants.edit',$contestant->id)}}" :contestant="{{$contestant}}" :key="{{$contestant->id}}"></contestant-card>
 		@endforeach
 	</div>
 	<div class="row">
-		<a class="btn btn-wedcontest is-green w-full" href="{{route('contestant.create')}}">@lang('contestants.addNew')</a>
+		<a class="btn btn-wedcontest is-green w-full" href="{{route('mycontestant.create')}}">@lang('contestants.addNew')</a>
 	</div>
 </div>
 @endsection    
--- routes/web.php
+++ routes/web.php
@@ -13,48 +13,56 @@
 use Illuminate\Mail\Markdown;
 
 Route::get('mail', function () {
-    $markdown = new Markdown(view(), config('mail.markdown'));
+	$markdown = new Markdown(view(), config('mail.markdown'));
 
-    return $markdown->render('emails.confirmar-email', ['user'=>App\User::first(), 'logo'=>'logo']);
+	return $markdown->render('emails.confirmar-email', ['user'=>App\User::first(), 'logo'=>'logo']);
 });
 Route::get('test', 'TestController@test');
 Route::get('lang/{route}/{language}', 'LanguageController@switchLang')->name('lang.switch');
 
 Route::post('/images/upload', 'ImageController@store')->middleware('admin')->name('store.image');
 Route::post('contest', 'ContestController@store')->middleware('admin')->name('contest.store');
+
 Route::put('contest/{contest}', 'ContestController@update')->middleware('admin')->name('contest.update');
 
 Route::group([
-    'prefix'     => LaravelLocalization::setLocale(),
+	'prefix'     => '/admin',
+    'middleware' => ['admin'], // Route admin 
+
+],function(){
+	Route::get('/contestants', 'ContestantController@index')->name('contestants.index');
+	Route::get('/contestant/create', 'ContestantController@create')->name('mycontestant.create');
+	Route::post('/contestant/{contestant}/edit', 'ContestantController@edit')->name('contestant.edit');
+	Route::put('/contestant/{contestant}', 'ContestantController@update')->name('contestant.update');
+});
+Route::group([
+	'prefix'     => LaravelLocalization::setLocale(),
     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'localize'], // Route translate middleware
 
 ], function () {
     // Route::resource('contest', 'ContestController');
-    Auth::routes();
-    Route::redirect('/', 'the_contest');
-    Route::get(LaravelLocalization::transRoute('routes.the_contest'), 'TheContestController@index')
-    ->name('the_contest');
-    Route::get('contests', 'ContestController@index')->middleware('admin')->name('contests.index');
-    Route::get('contest', 'ContestController@create')->middleware('admin')->name('contests.index');
-    Route::post('contest', 'ContestController@store')->middleware('admin')->name('contests.index');
-    Route::get('contest/create', 'ContestController@create')->middleware('admin')->name('contest.create');
-    Route::get('contest/{contest}', 'ContestController@show')->middleware('admin')->name('contest.show');
-    Route::get('contest/{contest}/edit', 'ContestController@edit')->middleware('admin')->name('contest.edit');
+	Auth::routes();
+	Route::redirect('/', 'the_contest');
+	Route::get(LaravelLocalization::transRoute('routes.the_contest'), 'TheContestController@index')
+	->name('the_contest');
+	Route::get('contests', 'ContestController@index')->middleware('admin')->name('contests.index');
+	Route::get('contest', 'ContestController@create')->middleware('admin')->name('contests.index');
+	Route::post('contest', 'ContestController@store')->middleware('admin')->name('contests.index');
+	Route::get('contest/create', 'ContestController@create')->middleware('admin')->name('contest.create');
+	Route::get('contest/{contest}', 'ContestController@show')->middleware('admin')->name('contest.show');
+	Route::get('contest/{contest}/edit', 'ContestController@edit')->middleware('admin')->name('contest.edit');
 
     // Route::resource('contests', 'ContestController');
 
-    Route::get('contest/{contest}/categories', 'CategoriesController@index')->middleware('admin')->name('categories');
+	Route::get('contest/{contest}/categories', 'CategoriesController@index')->middleware('admin')->name('categories');
 
-    Route::get('/register/confirm', 'Auth\RegisterConfirmationController@index')->name('register.confirm');
-    Route::get('/profile/edit', 'ProfileController@edit')->name('profile.edit');
-    Route::post('/profile', 'ProfileController@update')->name('profile.update');
-    Route::post('/profile', 'ProfileController@index')->name('profile.index');
-    Route::get('/mycontestants', 'MyContestantController@index')->name('mycontestants.index');
-    Route::post('/mycontestant', 'MyContestantController@store')->middleware('auth')->name('mycontestant.store');
-    Route::get('/mycontestants/{contestant}/edit', 'MyContestantController@edit')->name('mycontestants.edit');
-    Route::put('/mycontestants/{contestant}', 'MyContestantController@update')->name('mycontestants.update');
-    Route::get('/mycontestant/create', 'MyContestantController@create')->middleware('auth')->name('mycontestant.create');
-    Route::post('/contestant/{contestant}/edit', 'ContestantController@edit')->name('contestant.edit');
-    Route::put('/contestant/{contestant}', 'ContestantController@update')->name('contestant.update');
-    Route::get('/contestants', 'ContestantController@index')->name('contestants.index');
+	Route::get('/register/confirm', 'Auth\RegisterConfirmationController@index')->name('register.confirm');
+	Route::get('/profile/edit', 'ProfileController@edit')->name('profile.edit');
+	Route::post('/profile', 'ProfileController@update')->name('profile.update');
+	Route::post('/profile', 'ProfileController@index')->name('profile.index');
+	Route::get('/mycontestants', 'MyContestantController@index')->name('mycontestants.index');
+	Route::post('/mycontestant', 'MyContestantController@store')->middleware('auth')->name('mycontestant.store');
+	Route::get('/mycontestants/{contestant}/edit', 'MyContestantController@edit')->name('mycontestants.edit');
+	Route::put('/mycontestants/{contestant}', 'MyContestantController@update')->name('mycontestants.update');
+	Route::get('/mycontestant/create', 'MyContestantController@create')->middleware('auth')->name('mycontestant.create');
 });
