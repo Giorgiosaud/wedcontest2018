161da8720b5a07cb376a17a4363e71ebb9c9d9e2
Jorge Saud <jorgelsaud@gmail.com>
2018-06-13 20:10:12 -0400

    minor fixes ot update contestant

--- app/Http/Controllers/MyContestantController.php
+++ app/Http/Controllers/MyContestantController.php
@@ -69,7 +69,7 @@ class MyContestantController extends Controller
         $activeContest = Contest::whereActive(true)->first();
         $contestCatsId = Contest::whereActive(true)->first()->categories->pluck('id');
         $contestant->category()->detach($contestCatsId);
-        // dd($contestant);
+        dd($contestant);
         $contestant->update($contestantUpdate);
         if (request('email')) {
             Newsletter::subscribe(request('email'), ['firstName'=>request('name'), 'lastName'=>request('last_Name')], 'contestants');
