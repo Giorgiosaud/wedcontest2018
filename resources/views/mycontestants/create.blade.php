b3e8caf8856492957d5e00d92da0df960d52031b
Giorgiosaud <jorgelsaud@gmail.com>
2018-06-20 22:48:04 -0400

    lock unconfirmed emails

--- resources/views/mycontestants/create.blade.php
+++ resources/views/mycontestants/create.blade.php
@@ -2,8 +2,17 @@
 @section('content')
 @include('layouts.assets.headerStandart')
 <div class="pt-6">
-   <div class="content">
-   <contestant-registration-form post-to="{{ route('mycontestant.store') }}" :categories="{{$categories}}"></contestant-registration-form>
-   </div>
+	<div class="content">
+		@if(auth()->user()->confirmed)
+		<contestant-registration-form post-to="{{ route('mycontestant.store') }}" :categories="{{$categories}}"></contestant-registration-form>
+		@else
+		<div class="container">
+			<div class="col-6-offset-3">
+				<p class="border-dashed border-light p-3">@lang('contests.confirm_email')</p>
+			</div>
+		</div>
+		@endif
+
+	</div>
 </div>
 @endsection
