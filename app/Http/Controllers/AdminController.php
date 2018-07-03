030249a832501bcc530d91e0d52b5d860be0a078
Jorge Saud <jsaud@modyo.com>
2018-07-03 10:08:09 -0400

    Apply fixes from StyleCI (#31)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,24 +8,32 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-	public function translateActive(){
-		return view('translate.index');
-	}
-	public function translate(Contest $contest){
-		$contest->load('categories.artworks')->get();
-		$catsId = $contest->categories->pluck('id');
-		$artworks = Artwork::whereIn('category_id', $catsId)->whereState('approved')->orWhere('State','translated')->get();
-		return view('translate.bulk',compact('contest','artworks'));
-	}
-	public function SaveTranslation(Artwork $artwork){
-		// dd(request()->en);
-		$artwork->fill([
-			'en'=>request()->en,
-			'es'=>request()->es
-		]);
-		$artwork->state="translated";
-		$artwork->save();
-		return $artwork;
-	}
+    public function translateActive()
+    {
+        return view('translate.index');
+    }
+
+    public function translate(Contest $contest)
+    {
+        $contest->load('categories.artworks')->get();
+        $catsId = $contest->categories->pluck('id');
+        $artworks = Artwork::whereIn('category_id', $catsId)->whereState('approved')->orWhere('State', 'translated')->get();
+
+        return view('translate.bulk', compact('contest', 'artworks'));
+    }
+
+    public function SaveTranslation(Artwork $artwork)
+    {
+        // dd(request()->en);
+        $artwork->fill([
+            'en'=> request()->en,
+            'es'=> request()->es,
+        ]);
+        $artwork->state = 'translated';
+        $artwork->save();
+
+        return $artwork;
+    }
+
     //
 }
