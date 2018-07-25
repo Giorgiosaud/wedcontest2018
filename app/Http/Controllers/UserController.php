db7874a5d081b3ac709e5d2f2947cdf1c0b5b506
Giorgiosaud <jorgelsaud@gmail.com>
2018-07-25 10:26:57 -0400

    add aliasify to test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,6 @@ class UserController extends Controller
         return view('users.index');
     }
     public function aliasify(User $user){
-        dd($user);
         Auth::loginUsingId($user->id, true);
         return redirect('/the_contest');
 
