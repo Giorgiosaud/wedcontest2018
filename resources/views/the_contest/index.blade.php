030290c5367e7f982886075dcb5ee81c04e4203a
Giorgiosaud <jorgelsaud@gmail.com>
2018-07-29 23:36:04 -0400

    add message to home

--- resources/views/the_contest/index.blade.php
+++ resources/views/the_contest/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container text-center my-4">
         <img src="/storage/{{$contest->logo_image}}" alt="{{$contest->topic}}" style="max-width:300px">
 
-        @if(\Carbon\Carbon::create(2018, 7, 30, 0, 0, 0, 'America/Toronto')->gt(\Carbon\Carbon::now()))
+        @if(\Carbon\Carbon::create(2018, 7, 30, 0, 0, 0, 'America/Toronto')->lt(\Carbon\Carbon::now()))
         <h1 class="my-4 text-light">@lang('lang.header_closed')</h1>
         <p class="my-4 text-light text-center">@lang('lang.header_closed_mini')</h1>
         @else
@@ -33,8 +33,12 @@
         <p class="border-dashed border-light p-3">@lang('contests.confirm_email')</p>
         @endif
         @else
-        <a class="btn btn-primary btn-wedcontest mr-4" href="{{route("login")}}">@lang('contests.login_button')</a>
+        <a class="btn btn-primary btn-wedcontest @if(\Carbon\Carbon::create(2018, 7, 30, 0, 0, 0, 'America/Toronto')->gt(\Carbon\Carbon::now())) mr-4 @endif" href="{{route("login")}}">@lang('contests.login_button')</a>
+        @if(\Carbon\Carbon::create(2018, 7, 30, 0, 0, 0, 'America/Toronto')->gt(\Carbon\Carbon::now()))
+
         <a class="btn btn-primary btn-wedcontest mr-4" href="{{route("register")}}" >@lang('contests.register_button')</a>
+
+        @endif
         @endif
     </div>
     <div class="d-flex justify-content-center">
