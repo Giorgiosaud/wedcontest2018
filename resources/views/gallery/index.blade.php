73c5bd1105a412c0d7f4dfbe26aaa282e065ba89
Giorgiosaud <jorgelsaud@gmail.com>
2018-08-04 10:22:20 -0400

    modifications to gallery

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 @section('content')
-<div class="d-flex flex-column h-mid align-items-center justify-content-center text-light" style="background-image:url({{$contest->background_image}});    background-attachment: fixed;">
+<div class="d-flex flex-column h-mid align-items-center justify-content-center text-light gallery-bg" >
 	<div class="container text-center">
         <img src="/storage/{{$contest->logo_image}}" width="300" alt="Reinventando el Plástico">
-        <h1 class="">@lang('lang.wedGallery')</h1>
+        <h1 class="text-light">@lang('lang.wedGallery')</h1>
     </div>
 </div>
 <div class="pt-6">
