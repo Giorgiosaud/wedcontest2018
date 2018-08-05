acd0bae45e1b50dce314f749f3873e281fe04ae2
Giorgiosaud <jorgelsaud@gmail.com>
2018-08-05 11:07:08 -0400

    improve artwork permissions

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -8,17 +8,16 @@ class GalleryController extends Controller
 {
     public function index()
     {
-        // $contestToShow=Contest::with('categories.artworks')->whereActive(true)->get();
-        // ,compact('contestToShow')
-        return view('gallery.index');
-    }
+       $contestToShow=Contest::with('categories.artworks')->whereActive(true)->get()->last();
+       return view('gallery.show', ['contest'=>$contestToShow]);    
+   }
 
-    public function show(Contest $contest)
-    {
+   public function show(Contest $contest)
+   {
         // $contestToShow=Contest::with('categories.artworks')->whereActive(true)->get();
         // ,compact('contestToShow')
-        return view('gallery.show', compact('contest'));
-    }
+    return view('gallery.show', compact('contest'));
+}
 
     //
 }
