e53a68cd34170628c9195a48a3f42fdce80a589a
Giorgiosaud <jorgelsaud@gmail.com>
2018-09-07 21:06:59 -0300

    plain gallery bg white

--- resources/views/gallery/show2.blade.php
+++ resources/views/gallery/show2.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.plain')
 @section('content')
+<style>
+	body{
+		background: #fff;
+	}
+</style>
 <div class="pt-6">
    <div class="content py-4">
    	<gallery-plain :contest="{{$contest}}">
