df011f88787bfdc635d102470c6ddcc91335b712
Jorge Saud <giorgiosaud@gmail.com>
2018-11-05 15:04:13 -0300

    add excel export (#42)
    * add excel export
    
    * add excel export

---
+++ app/Exports/UsersExport.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Exports;
+
+use App\User;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+
+class UsersExport implements FromView
+{
+    public function view(): View
+    {
+       $users= User::with('contestants')->get();
+        return view('users.table',compact('users'));
+    }
+}
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 @section('content')
 @include('layouts.assets.noTextHeaderStandart')
+
 <div class="pt-6">
+<a href="http://contest.diproinduca.com/admin/users/export">download</a>
    <div class="content">
    	<users></users>
    </div>
